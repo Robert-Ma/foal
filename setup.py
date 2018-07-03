0592c95e70fe60c6a13f1d4be0233cee1e3f9bb0
Robert Ma <Ma-Wei@outlook.com>
2018-07-03 20:06:37 +1000

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='basicalgorithms',
+    version='0.1.0',
+    license='MIT',
+    description='Basic Algorithms with Python',
+
+    author='Wei Ma',
+    author_email='Wei-Ma@outlook.com.au',
+    url=None,
+
+    packges=find_packages('eagle'),
+    package_dir={'': 'eagle'},
+    install_requires=[],
+)
