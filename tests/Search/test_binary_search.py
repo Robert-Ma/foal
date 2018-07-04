7bfc6b8f24f4f6dd6a516573eb2397fd6a952183
Robert Ma <Ma-Wei@outlook.com>
2018-07-04 20:45:36 +1000

    add test for binary_search

--- tests/Search/test_binary_search.py
+++ tests/Search/test_binary_search.py
@@ -1,11 +1,18 @@
+import eagle.Search
 import pytest
-import basalg
 
 
 def test_search_element_in_seq():
-    S = [1, 5, 2, 6, 9]
-    assert basalg.binary_search(S, 2) is True
+    S = [1, 2, 5, 6, 9]
+    assert eagle.Search.binary_search(S, 2) == 1
 
 
 def test_search_element_not_in_seq():
-    pass
+    S = [1, 2, 5, 6, 9]
+    assert eagle.Search.binary_search(S, 10) == -1
+
+
+def test_search_with_unsorted_list():
+    S = [2, 1, 6, 3, 8]
+    with pytest.raises(ValueError):
+        eagle.Search.binary_search(S, 3)
