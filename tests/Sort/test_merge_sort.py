4c79c1cdb36d500cb244663bb9ed2ef619b096fc
Robert Ma <Wei-Ma@outlook.com.au>
2018-07-06 23:33:27 +1000

    refactor: clean repo
    Delete some *.pyc files from repo

--- tests/Sort/test_merge_sort.py
+++ tests/Sort/test_merge_sort.py
@@ -9,3 +9,13 @@ def test_merge_sort_no_repeat_items():
 def test_merge_sort_with_repeat_items():
     S = [27, 22, 10, 12, 20, 25, 13, 15, 12, 22]
     assert eso.merge_sort(S) == [10, 12, 12, 13, 15, 20, 22, 22, 25, 27]
+
+
+def test_with_only_one_element():
+    S = [1]
+    assert eso.merge_sort(S) == [1]
+
+
+def test_with_zero_element():
+    S = []
+    assert eso.merge_sort(S) == []
