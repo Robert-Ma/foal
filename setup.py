7f7fa4f76d0f1550b9a1bf80b3e6dd46c7c0ee49
Robert Ma <Ma-Wei@outlook.com>
2018-07-07 18:34:17 +1000

    docs: update setup.py
    Add long_description

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ setup(
     version='0.1.0',
     license='MIT',
     description='Foundations of Algorithms with Python3',
+    long_description=open('README.md', 'r').read(),
 
     author='Wei Ma',
     author_email='Wei-Ma@outlook.com.au',
-    url=None,
+    url='https://github.com/Robert-Ma/foal',
 
-    packges=find_packages(where='src'),
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[],
 )
