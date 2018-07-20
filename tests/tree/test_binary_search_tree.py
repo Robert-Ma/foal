92bf156a4a627a09a8496b8ce1db746ac22f3dc5
Robert Ma <Ma-Wei@outlook.com>
2018-07-20 19:39:04 +1000

    test: add tests for binary search tree
    Tests for:
    (1) delete one value
        (i) delete leaf node
        (ii) delete parent node
        (iii) delete root node
    (2) compare two binary search trees
        (i) __eq__()
        (ii) __ne__()

--- tests/tree/test_binary_search_tree.py
+++ tests/tree/test_binary_search_tree.py
@@ -16,7 +16,7 @@ def test_tree_build(init_tree):
 
     sys.stdout = old_stdout
 
-    assert(result.getvalue() == '1, 3, 8, 10, ')
+    assert(result.getvalue() == '5, 10, 15, 20, 25, 30, 35, ')
 
 
 def test_search_item_in_tree(init_tree):
@@ -31,11 +31,83 @@ def test_search_item_not_in_tree(init_tree):
     assert search_result == expected
 
 
+def test_delete_leaf(init_tree):
+    tree = init_tree
+    tree.delete(5)
+
+    expected_tree = BinarySearchTree()
+    expected_tree.insert(20)
+    expected_tree.insert(10)
+    expected_tree.insert(15)
+    expected_tree.insert(30)
+    expected_tree.insert(25)
+    expected_tree.insert(35)
+
+    assert tree == expected_tree
+
+
+def test_delete_parent(init_tree):
+    tree = init_tree
+    tree.delete(10)
+
+    expected_tree = BinarySearchTree()
+    expected_tree.insert(20)
+    expected_tree.insert(15)
+    expected_tree.insert(5)
+    expected_tree.insert(30)
+    expected_tree.insert(25)
+    expected_tree.insert(35)
+
+    assert tree == expected_tree
+
+
+def test_delete_root(init_tree):
+    tree = init_tree
+    tree.delete(20)
+
+    expected_tree = BinarySearchTree()
+    expected_tree.insert(30)
+    expected_tree.insert(25)
+    expected_tree.insert(10)
+    expected_tree.insert(5)
+    expected_tree.insert(15)
+    expected_tree.insert(35)
+
+    assert tree == expected_tree
+
+
+def test_two_trees_equal(init_tree):
+    t2 = BinarySearchTree()
+    t2.insert(20)
+    t2.insert(10)
+    t2.insert(5)
+    t2.insert(15)
+    t2.insert(30)
+    t2.insert(25)
+    t2.insert(35)
+    assert t2 == init_tree
+
+
+def test_two_trees_not_equal(init_tree):
+    print(init_tree)
+    t2 = BinarySearchTree()
+    t2.insert(20)
+    t2.insert(10)
+    t2.insert(5)
+    t2.insert(15)
+    t2.insert(30)
+    t2.insert(25)
+    assert t2 != init_tree
+
+
 @pytest.fixture()
 def init_tree():
     t = BinarySearchTree()
-    t.insert(3)
+    t.insert(20)
     t.insert(10)
-    t.insert(8)
-    t.insert(1)
+    t.insert(5)
+    t.insert(15)
+    t.insert(30)
+    t.insert(25)
+    t.insert(35)
     return t
