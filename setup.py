729d4122f98ddc59b6c6252e59a87bf2ba3ef496
Robert Ma <Ma-Wei@outlook.com>
2018-07-20 20:43:43 +1000

    change the version number
    Add tag: v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='foal',
-    version='0.1.0',
+    version='0.1.1',
     license='MIT',
     description='Foundations of Algorithms with Python3',
     long_description=open('README.md', 'r').read(),
